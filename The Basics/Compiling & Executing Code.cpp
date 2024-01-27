98b21960337be3b382c335fea01908d939c7ee4b
Austin <47762009+acrumx@users.noreply.github.com>
2024-01-26 23:04:51 -0800

    Create Compiling & Executing Code.cpp

---
+++ The Basics/Compiling & Executing Code.cpp
@@ -0,0 +1,20 @@
+// file name in this scenario is gallops.cpp 
+
+#include <iostream>
+
+int main(){
+  std::cout << "Did you know that C++ supports various different programming styles!?";
+}
+
+
+
+
+// The below is what you would type into the terminal to compile & execute the C++ file.
+// Important thing to take note of: The computer itself does not know C++, you
+// will be turning it into machine code (binary). 
+
+// You must have the terminal's current working directory, set to the same as 
+// that of the C++ program you are trying to compile and execute. 
+
+// g++ gallus.cpp <- This will take the C++ file, compile it (resulting in an a.out file)
+// ./a.out <- This will execute the a.out file you compiled. 
