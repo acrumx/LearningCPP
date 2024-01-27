a94b3f5ee0ebc355957d74d6bf9dc0001d5ef1fa
Austin <47762009+acrumx@users.noreply.github.com>
2024-01-26 22:36:15 -0800

    Update hello_world.cpp
    Added some comments to clarify what some of the code means (helpful to other beginners)

--- The Basics/hello_world.cpp
+++ The Basics/hello_world.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
+#include <iostream> //  The iostream library allows you to use inputs, and give outputs
 
 int main ()    {
-  std::cout << "Hello world!\n";
+  std::cout << "Hello world!\n"; // this prints the text within parenthesis on the screen
   std::cout << "It's a pleasure meeting all of you...\n";
   std::cout << "I hope you enjoy following my C++ journey.";
+  return 0; // once the "return" function/line of code is ran - the program will "terminate" itself afterwards (it'll stop running)
 }
