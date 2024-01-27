c6724b780c1039cef28ccda5661f09c3f97f520c
Austin <47762009+acrumx@users.noreply.github.com>
2024-01-26 22:31:17 -0800

    Create hello_world.cpp
    A basic "hello world"

---
+++ The Basics/hello_world.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main ()    {
+  std::cout << "Hello world!\n";
+  std::cout << "It's a pleasure meeting all of you...\n";
+  std::cout << "I hope you enjoy following my C++ journey.";
+}
